e9a2398ab37d910a4b7cb08ca623c506d32b2626
Charles Ellis <hamled@hamled.name>
2016-11-13 01:41:46 -0800

    Player must be given a name

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -6,5 +6,11 @@ describe('Player', function() {
     it('can be called', function() {
       expect(Player).toBeFunction();
     });
+
+    it('throws an Error when not given a name argument', function() {
+      expect(function() {
+        return new Player();
+      }).toThrowError(Error, 'Player must be given a name');
+    });
   });
 });
--- src/player.js
+++ src/player.js
@@ -1,3 +1,7 @@
-var Player = function() {};
+var Player = function(name) {
+  if(name === undefined) {
+    throw new Error('Player must be given a name');
+  }
+};
 
 export default Player;

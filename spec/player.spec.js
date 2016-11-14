a225b08be8bac3b7b93d9f23cdc95fd67a6fe7e3
Charles Ellis <hamled@hamled.name>
2016-11-14 07:18:57 -0800

    highestWordScore is score from word in #plays spec

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -301,5 +301,20 @@ describe('Player', function() {
 
       expect(player.highestWordScore()).toEqual(highestScore);
     });
+
+    it('returns a score matching the score of a word within #plays array', function() {
+      var player = new Player('Player 1');
+      var words = [
+        'cranberry',
+        'crowberry',
+        'cloudberry'
+      ];
+      var scores = _.map(words, Scrabble.score);
+
+      // Play all the words
+      _.each(words, _.bind(player.play, player));
+
+      expect(scores).toContain(player.highestWordScore());
+    });
   });
 });
